50b9cc0b3f4dbfa4cd4f2836d24144965fe8b905
Ankitha <ankicool20@gmail.com>
2017-10-03 14:11:35 -0400

    Project 2 proposal baseline implementation with counter

--- Baseline.py
+++ Baseline.py
@@ -1,5 +1,5 @@
 def file_reader(file_path, list_lines ):
-    print("Reading file into list of lines")
+    print("Reading files into list of lines")
     file_stream = open(file_path, 'r')
     with file_stream as in_file:
         for lines in in_file:
@@ -7,51 +7,63 @@ def file_reader(file_path, list_lines ):
     return list_lines
 
 
-def word_dictionary_builder(list_lines, data):
-    print(" Building word Dictionary for " + data + " data")
+def word_dictionary_builder(list_lines):
+    print(" Building word Dictionary for training data")
     word_dictionary = {}
     for i in range(0, len(list_lines) - 2, 3):
         word_tuple = list(zip(list_lines[i], list_lines[i + 1], list_lines[i + 2]))
         for word, pos, ner in word_tuple:
-           word_dictionary[word] = (pos, ner)
+            if word in word_dictionary.keys():
+                p, n, counter = word_dictionary[word]
+                counter += 1
+                word_dictionary[word] = (p, n, counter)
+            else:
+                word_dictionary[word] = (pos, ner, 0)
     return word_dictionary
+
+
+def word_builder_for_test(test_lines, word_dictionary):
+    print("Building list of tuples for test data")
+    test_ner = []
+    for i in range(0, len(test_lines) - 2, 3):
+        word_tuple_test = list(zip(test_lines[i], test_lines[i + 1], test_lines[i + 2]))
+        for word, pos, loc in word_tuple_test:
+            if word in word_dictionary.keys():
+                pos, ner, counter = word_dictionary[word]
+                test_ner.extend([(word,ner,loc)])
+    return test_ner
+
 
 def main():
     file_path_train = "D:\PythonProjects\\NamedEntityRecognition\\trainingData\\train.txt"
     file_path_test = "D:\PythonProjects\\NamedEntityRecognition\\testData\\test.txt"
 
     list_lines_train = file_reader(file_path_train, [])
-    word_dict_train = word_dictionary_builder(list_lines_train, "Training")
+    word_dict_train = word_dictionary_builder(list_lines_train)
 
     list_lines_test = file_reader(file_path_test, [])
-    word_dict_test = word_dictionary_builder(list_lines_test, "Test")
-    #print(list_lines_test)
-    print(word_dict_train.keys())
-    print(word_dict_test.keys())
-    keys_not_found = []
-    ner_position_dict = {'ORG':[-1], 'MISC':[-1], 'PER':[-1], 'LOC':[-1]}
-    for key in word_dict_test.keys():
-        if key in word_dict_train.keys():
-            pos1, ner = word_dict_train[key]
-            pos2, location = word_dict_test[key]
-            ner = ner[2:]
-            if ner in ner_position_dict.keys():
-                if ner_position_dict[ner]:
-                    ner_position_dict[ner].extend([location])
+    word_tuples_test = word_builder_for_test(list_lines_test,word_dict_train)
+
+    words_not_found = []
+    ner_position_dict = {'ORG': [-1], 'MISC': [-1], 'PER': [-1], 'LOC': [-1]}
+
+    for word, ner, loc in word_tuples_test:
+        ner = ner[2:]
+        if ner in ner_position_dict.keys():
+            if ner_position_dict[ner]:
+                ner_position_dict[ner].extend([loc])
         else:
-            keys_not_found.append(key)
-    print(len(keys_not_found))
+            words_not_found.append(word)
 
     for key, val in ner_position_dict.items():
         integer_list_values = [int(v) for v in val]
-        ner_position_dict[key] =sorted(integer_list_values)
+        ner_position_dict[key] = sorted(integer_list_values)
 
     for k,v in ner_position_dict.items():
         ranges = sum((list(t) for t in zip(v, v[1:]) if t[0] + 1 != t[1]), [])
         iranges = iter(v[0:1] + ranges + v[-1:])
         ner_position_dict[k] = ' '.join([str(n) + '-' + str(next(iranges)) for n in iranges])
 
-    #print(ner_position_dict)
     import csv
     with open('namedEntityRecon.csv', 'w') as csvfile:
         header = ['Type', 'Prediction']
@@ -64,5 +76,3 @@ def main():
 if __name__ == "__main__":
         main()
 
-#2831
-#3026
---
+++ Readme.md
@@ -0,0 +1,8 @@
+Attached are two implementations of the Baseline for  NER.
+
+Baseline.py(Python 3.X) Gives accurracy of 67.505%
+To run the code,path of the train and test data that are currently hardcoded and needs to be modified.
+Generates the csv file "namedEntityRecon.csv" for Kaggle submissions 
+
+BaselineImpl2.py (Python(2.x) Gives Accuracy of 62.796%
+Can be run directly. No modifications required
